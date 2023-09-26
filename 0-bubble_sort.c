e876ef651e4d676c6dac38bd125c90f5aebff6ed
Ella4040 <usouella@gmail.com>
2023-09-25 17:24:55 -0700

    udapte

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,26 +7,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, n;
-    int tmp, swap;
+	size_t i, n;
+	int tmp, swap;
 
-    /*Main loop*/
-    for (n = size, swap = 1; n > 0 && swap; n--)
-    {
-        swap = 0; // Initialize at 0, no swaps at beginning
+	/*Main loop*/
+	for (n = size, swap = 1; n > 0 && swap; n--)
+	{
+		swap = 0; /*Initialize at 0, no swaps at beginning*/
 
-        /* Scans array up to n-1*/
-        for (i = 0; (i + 1) < n; i++)
-        {
-            /*If the current element is larger than the next, swap*/
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i + 1];
-                array[i + 1] = array[i];
-                array[i] = tmp;
-                print_array(array, size); /*Call a function to display the array*/
-                swap = 1; /*There's been a swap*/
-            }
-        }
-    }
+		/* Scans array up to n-1*/
+		for (i = 0; (i + 1) < n; i++)
+		{
+		/*If the current element is larger than the next, swap*/
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size); /*Call a function to display the array*/
+				swap = 1; /*There's been a swap*/
+            		}
+        	}
+    	}
 }
